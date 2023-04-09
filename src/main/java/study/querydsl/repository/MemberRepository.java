ea9a4e3e1e10f40001e8cc150ef190acd987689a
정상화 <jungsh930210@gmail.com>
2023-04-10 01:43:47 +0900

    edit

---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+    List<Member> findByUsername(String username);
+
+}
